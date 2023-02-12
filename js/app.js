7f1226ad03049293112d1d4ee71f2b253f910e29
Kameron Watts <kam.watts@gmail.com>
2023-02-11 21:02:29 -0800

    changed background and nav colors

--- js/app.js
+++ js/app.js
@@ -13,23 +13,29 @@ console.log("Hello There");
 let myContainer = document.querySelector("section");
 let myButton = document.querySelector("section + div");
 
+// target the images through css
 let image1 = document.querySelector("section img:first-child");
 let image2 = document.querySelector("section img:nth-child(2)");
 let image3 = document.querySelector("section img:nth-child(3)");
 
+// keep track of these numbers globally for comparisons and data storage
 let numberOfMatches = 0;
 let numberOfMatchesAllowed = 25; 
 
+// stores the product instances
 let allProducts = [];
+
+// stores the random images of the products
 let indexArray = [];
 
+// contructor function for each Product instance
 function Products(name, fileExtension = "jpg") {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
   this.imageShown = 0;
   this.likes = 0;
 }
-
+// The actual products called as new instances of the Products constructor
 let bag = new Products("bag");
 let banana = new Products("banana");
 let bathroom = new Products("bathroom");
@@ -50,6 +56,7 @@ let unicorn = new Products("unicorn");
 let watercan = new Products("water-can");
 let wineglass = new Products("wine-glass");
 
+// array that stores the products
 allProducts = [
   bag,
   banana,
@@ -80,17 +87,18 @@ function getProducts () {
   console.log(allProducts);
   }
 
-  // turns the allProducts array into a string to save the data as an object in local storage
+// turns the allProducts array into a string to save the data as an object in local storage
 function storeProduct() {
   let stringifiedItems = JSON.stringify(allProducts);
   localStorage.setItem('allProducts', stringifiedItems);
 }
-
+// math to produce random images retreived from the allproducts array
 function selectRandomImage() {
   return Math.floor(Math.random() * allProducts.length);
 }
 getProducts(); // calls our getProducts function
 
+// shows the photos. pushes to the indexArray if the photos aren't there, populates 3 photos of the products.
 function renderProducts() {
 
   while (indexArray.length < 6) {
@@ -110,12 +118,14 @@ function renderProducts() {
   image1.alt = allProducts[product1].name;
   image2.alt = allProducts[product2].name;
   image3.alt = allProducts[product3].name;
-
+  
+  // Increases the views tally
   allProducts[product1].imageShown++;
   allProducts[product2].imageShown++;
   allProducts[product3].imageShown++;
 }
 
+// What happens when the photo is clicked. Tracks the name of the photo and if that photo is clicked, the name will be registered into the console
 function handleClick(event) {
   let clickedProduct = event.target.alt;
   for (let i = 0; i < allProducts.length; i++) {
@@ -123,7 +133,7 @@ function handleClick(event) {
       allProducts[i].likes++;
     }
   }
-
+  // keeps track of the maximum number of attempts in relation to the matches. Keeps track of how many times a photo is matched (picked more than once)
   if (numberOfMatches < numberOfMatchesAllowed) {
     renderProducts();
     numberOfMatches++;
@@ -136,6 +146,7 @@ function handleClick(event) {
 }
 myContainer.addEventListener("click", handleClick);
 
+// this chart will render with all of the data the local storage is collecting
 function showChart() {
   let productLikes = [];
   let productNames = [];
@@ -150,6 +161,7 @@ function showChart() {
   console.log(productLikes);
   console.log(productViews);
 
+  // Chart.js code that styles the chart
   let config = {
     type: 'bar',
     data: {
